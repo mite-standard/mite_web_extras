af35df0d03ed30f3d5eb665ef6fd6d4b92effc59
Mitja M. Zdouc <mmzdouc@gmail.com>
2026-03-03 17:40:22 +0100

    Debugging CI/CD

--- entrypoint_docker.sh
+++ entrypoint_docker.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # run data download script
+echo "Started mite_web_extras"
 python -m mite_web_extras.main
 
+echo "Started image generation"
 # run image generation script
 ./mite_web_extras/run_pymol.sh
 
+echo "Started cleanup generation"
 # run cleanup script
 python -m mite_web_extras.cleanup
